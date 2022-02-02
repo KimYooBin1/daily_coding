79adbccd654bd01365a617cba535cdc8ee30b53c
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2022-02-02 23:04:59 +0900

    클래스 문제

---
+++ c++/문제/3-2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Calculator
+{
+private:
+  int numofadd;
+  int numofdiv;
+  int numofmul;
+  int numofmin;
+public:
+  void Init(){
+      numofadd = 0;
+      numofmin = 0;
+      numofmul = 0;
+      numofdiv = 0;
+  }
+  double Add(double x, double y)
+  {
+    numofadd++;
+    return x+y;
+  }
+  double Div(double x, double y)
+  {
+    numofdiv++;
+    return x/y;
+  }
+  double Min(double x, double y)
+  {
+    numofmin++;
+    return x-y;
+  }
+  double Mul(double x, double y)
+  {
+    numofmul++;
+    return x*y;
+  }
+  void ShowOpCount()
+  {
+    cout<<"덧셈: "<<numofadd<<"뺄셈: "<<numofmin<<"곱셈: "<<numofmul<<"나눗셈: "<<numofdiv<<endl;
+  }
+};
+  
+int main(void)
+{
+  Calculator cal;
+  cal.Init();
+  cout<<"3.2 + 2.4 = "<<cal.Add(3.2, 2.4)<<endl;
+  cout<<"3.5 / 1.7 = "<<cal.Div(3.2, 2.4)<<endl;
+  cout<<"2.2 - 1.5 = "<<cal.Min(3.2, 2.4)<<endl;
+  cout<<"4.9 / 1.2 = "<<cal.Div(3.2, 2.4)<<endl;
+  cal.ShowOpCount();
+  return 0;
+}
