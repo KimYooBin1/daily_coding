6bc8e87fcafd1edac024e5adc2e673e3fe20eabf
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2022-02-07 23:08:46 +0900

    error

--- c++/문제/4-3.cpp
+++ c++/문제/4-3.cpp
@@ -1,22 +1,71 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
+
+namespace COMP_POS
+{
+    enum
+    {
+        CLERK,
+        SENIOR,
+        ASSIST,
+        MANAGER
+    };
+    
+    void ShowComp_Pos(int rank)
+    {
+        switch(rank)
+        {
+                case CLERK:
+                    cout<<"사원"<<endl;
+                case SENIOR:
+                    cout<<"주임"<<endl;
+                case ASSIST:
+                    cout<<"대리"<<endl;
+                case MANAGER:
+                    cout<<"과장"<<endl;
+        }
+    }
+}
 
 class NameCard
 {
 private:
-    char name[];
-    char ComName[];
-    char number[];
-    int rank;
-    enum {CLERK, SENIOR, ASSIST, MANAGER};
+    char *myname;
+    char *myComName;
+    char *mynumber;
+    int myrank;
 public:
-    NameCard(name, ComName, number, rank)
+    NameCard(char *name, char *ComName, char *number, int rank) : myrank(rank)
     {
-        cout<<"생성자 실행"<<endl;
+        int len1=strlen(name)+1;
+        myname = new char(len1);
+        strcpy(myname, name);
+        
+        int len2=strlen(ComName)+1;
+        myComName = new char(len2);
+        strcpy(myComName, ComName);
+        
+        int len3=strlen(number)+1;
+        myComName = new char(len3);
+        strcpy(mynumber, number);
+        
+        
     }
-    void ShowNameCardInfo();
+    void ShowNameCardInfo()
     {
-        cout<<"hello world"<<endl;
+        cout<<"이름: "<<myname<<endl;
+        cout<<"회사: "<<myComName<<endl;
+        cout<<"전화번호: "<<mynumber<<endl;
+        cout<<"사원: "; COMP_POS::ShowComp_Pos(myrank);
+        cout<<endl;
+        
+    }
+    ~NameCard()
+    {
+        delete []myname;
+        delete []myComName;
+        delete []mynumber;
     }
 };
 
@@ -24,9 +73,10 @@ int main(void)
 {
     NameCard manClerk("Lee", "ABCRng", "010-1111-2222", COMP_POS::CLERK);
     NameCard manSENIOR("Hong", "OrangeEng", "010-3333-4444", COMP_POS::SENIOR);
-    NameCard manAssist("Kim", "SoGoodComp", "010-5555-6666", COMP_POS::Assist);
+    NameCard manAssist("Kim", "SoGoodComp", "010-5555-6666", COMP_POS::ASSIST);
     manClerk.ShowNameCardInfo();
     manSENIOR.ShowNameCardInfo();
     manAssist.ShowNameCardInfo();
     return 0;
 }
+
