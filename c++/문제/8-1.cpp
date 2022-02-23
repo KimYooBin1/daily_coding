a3580252390d2ae0762dad5f3ad8408476360727
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2022-02-23 22:40:48 +0900

    Update 8-1.cpp

--- c++/문제/8-1.cpp
+++ c++/문제/8-1.cpp
@@ -190,11 +190,11 @@ int main()
     handler.AddEmployee(fseller1);
     
     ForeignSalesWorker * fseller2=new ForeignSalesWorker("Yoon", 1000, 0.1, RISK_LEVEL::RISK_B);
-    fseller1->AddSalesResult(7000);
+    fseller2->AddSalesResult(7000);
     handler.AddEmployee(fseller2);
     
     ForeignSalesWorker * fseller3=new ForeignSalesWorker("Lee", 1000, 0.1, RISK_LEVEL::RISK_C);
-    fseller1->AddSalesResult(7000);
+    fseller3->AddSalesResult(7000);
     handler.AddEmployee(fseller3);
     
     handler.ShowAllSalaryInfo();
