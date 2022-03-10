b9fbd5c86f27f22c33a00b44177c98c13f0f1bd5
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2022-03-10 22:51:15 +0900

    Create 5.cpp

---
+++ c/문제/5.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/*
+int main(void)
+{
+  int x1, y1, x2, y2;
+  int width, length;
+  scanf("%d %d %d %d/n", &x1, &y1, &x2, &y2);
+  width = x1-x2;
+  length = y1-y2;
+  printf("두 점이 이루는 직사각형의 넓이는 %d입니다", width*length);
+  return 0;
+}
+
+int main(void)
+{
+  double x, y;
+  scanf("%lf %lf", &x, &y);
+  printf("x + y = %f\n", x+y);
+  printf("x - y = %f\n", x-y);
+  printf("x * y = %f\n", x*y);
+  printf("x / y = %f\n", x/y);
+}
+
+int main(void)
+{
+  int input;
+  scanf("%d", &input);
+  printf("%c", input);
+}
+
+int main(void)
+{
+  char input;
+  scanf("%c", &input);
+  printf("%d", input);
+}
+*/
